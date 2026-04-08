8b80bd00214667712ccb97746d3988bbb7b89122
EylinJois <s160423031@student.ubaya.ac.id>
2026-04-08 08:19:12 +0700

    Modified DetailViewModel Volley

--- app/src/main/java/com/ubaya/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/ubaya/studentproject/viewmodel/DetailViewModel.kt
@@ -1,15 +1,24 @@
 package com.ubaya.studentproject.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
 import com.ubaya.studentproject.model.Student
 
-class DetailViewModel: ViewModel() {
+class DetailViewModel(application: Application): AndroidViewModel(application) {
     val studentLD= MutableLiveData<Student>()
+    val TAG:String = "Volley Tag"
+    var queue: RequestQueue?=null
 
     fun fetch(student: Student){
 //        val student1 = Student("16055","Nonie","1998/03/28","5718444778",
 //            "http://dummyimage.com/75x100.jpg/cc0000/ffffff")
+        queue= Volley.newRequestQueue(getApplication())
+        val url = "https://www.jsonkeeper.com/b/LLMW"
+
         studentLD.value = student
     }
 }
